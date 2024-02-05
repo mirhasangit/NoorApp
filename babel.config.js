8a190f7c84d8da6fda5bd43ba6d8594e6260df67
mirhasangit <mirhasabus@gmail.com>
2024-02-05 17:21:53 -0600

    updated

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    // Ensure plugins are correctly specified
+    'react-native-reanimated/plugin',
+  ],
 };
